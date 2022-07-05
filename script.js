f47d017400abcface0291f3f107da6b0a061672c
sarthakladhwe <sarthak.ladhwe156@gmail.com>
2022-07-05 22:33:59 +0530

    API call to fetch words

--- script.js
+++ script.js
@@ -4,3 +4,10 @@ const scoreEl = document.getElementById('score')
 const wordEl = document.getElementById('word')
 const wordInputEl = document.getElementById('word-input')
 const settingsBtn = document.getElementById('settings')
+
+async function getWords() {
+    const response = await fetch(`https://random-word-api.herokuapp.com/word?length=5&number=30`)
+    const data = await response.json()
+    wordEl.innerText = `${data[0]}`
+}
+
