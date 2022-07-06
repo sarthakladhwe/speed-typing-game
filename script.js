241e4ef92e0c116138598361cdac99d843318e46
sarthakladhwe <sarthak.ladhwe156@gmail.com>
2022-07-06 09:17:39 +0530

    Text input logic

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const scoreEl = document.getElementById('score')
 const wordEl = document.getElementById('word')
 const wordInputEl = document.getElementById('word-input')
 const settingsBtn = document.getElementById('settings')
+const startGameBtn = document.getElementById('start-game')
 
 let difficulty = "easy";
 let score = 0;
@@ -15,15 +16,40 @@ async function getWords() {
     const randomiseWordLength = wordLength - Math.floor(Math.random() * 3)
     console.log(randomiseWordLength)
     const response = await fetch(`https://random-word-api.herokuapp.com/word?length=${randomiseWordLength}`)
-    const data = await response.json() 
-    randomWord = data;  
+    const data = await response.json()
+    randomWord = data[0]
+    wordEl.textContent = randomWord
 }
 
-getWords()
+function startCountdown() {
+    const intervalId = setInterval(() => {
+        if(timeLeft <= 0) {
+            stopGame()
+            clearInterval(intervalId)
+        }
+        timeLeft--
+        timeLeftEl.innerText = `${timeLeft}s`
+    }, 1000)
+}
 
 //Event Listeners
 
 difficultyEl.addEventListener('change', (e) => {
     difficulty = e.target.value
+})
+
+startGameBtn.addEventListener('click', () => {
+    startGameBtn.style.visibility = "hidden"
+    getWords()
+    startCountdown()
+})
+
+wordInputEl.addEventListener('input', (e) => {
+    const insertedWord = e.target.value
+    if(insertedWord === randomWord) {
+        getWords()
+        wordInputEl.value = ""
+        score++
+    }
 })
 
