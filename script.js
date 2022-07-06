20aec1dea3f7439c2952a8e5f7f7f1fe8fda306a
sarthakladhwe <sarthak.ladhwe156@gmail.com>
2022-07-06 08:57:21 +0530

    Setting up difficulty

--- script.js
+++ script.js
@@ -5,9 +5,25 @@ const wordEl = document.getElementById('word')
 const wordInputEl = document.getElementById('word-input')
 const settingsBtn = document.getElementById('settings')
 
+let difficulty = "easy";
+let score = 0;
+let timeLeft = 10;
+let randomWord;
+
 async function getWords() {
-    const response = await fetch(`https://random-word-api.herokuapp.com/word?length=5&number=30`)
-    const data = await response.json()
-    wordEl.innerText = `${data[0]}`
+    const wordLength = difficulty === "easy" ? 5 : difficulty === "medium" ? 8 : 11;
+    const randomiseWordLength = wordLength - Math.floor(Math.random() * 3)
+    console.log(randomiseWordLength)
+    const response = await fetch(`https://random-word-api.herokuapp.com/word?length=${randomiseWordLength}`)
+    const data = await response.json() 
+    randomWord = data;  
 }
+
+getWords()
+
+//Event Listeners
+
+difficultyEl.addEventListener('change', (e) => {
+    difficulty = e.target.value
+})
 
