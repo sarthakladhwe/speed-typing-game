78f9f570cd03e0b01931826404d07179c27b20ce
sarthakladhwe <sarthak.ladhwe156@gmail.com>
2022-07-06 15:53:21 +0530

    Added Endgame functionality and restart

--- script.js
+++ script.js
@@ -6,6 +6,10 @@ const wordInputEl = document.getElementById('word-input')
 const settingsBtn = document.getElementById('settings')
 const startGameBtn = document.getElementById('start-game')
 
+const gameContainer = document.getElementById('start-game-container')
+const gameEndContainer = document.getElementById('stop-game-container')
+gameEndContainer.style.display = "none"
+
 let difficulty = "easy";
 let score = 0;
 let timeLeft = 10;
@@ -14,22 +18,30 @@ let randomWord;
 async function getWords() {
     const wordLength = difficulty === "easy" ? 5 : difficulty === "medium" ? 8 : 11;
     const randomiseWordLength = wordLength - Math.floor(Math.random() * 3)
-    console.log(randomiseWordLength)
     const response = await fetch(`https://random-word-api.herokuapp.com/word?length=${randomiseWordLength}`)
     const data = await response.json()
+    wordEl.textContent = data[0]
     randomWord = data[0]
-    wordEl.textContent = randomWord
 }
 
 function startCountdown() {
     const intervalId = setInterval(() => {
-        if(timeLeft <= 0) {
-            stopGame()
-            clearInterval(intervalId)
-        }
         timeLeft--
         timeLeftEl.innerText = `${timeLeft}s`
     }, 1000)
+    if(timeLeft <= 0) {
+        stopGame()
+        timeLeft = 0
+        clearInterval(intervalId)
+    }
+    console.log(timeLeft)
+}
+
+function stopGame() {
+    gameContainer.style.display = "none"
+    gameEndContainer.style.display = "block"
+    startGameBtn.style.visibility = "visible"
+    document.getElementById('final-score').textContent = score
 }
 
 //Event Listeners
@@ -42,6 +54,7 @@ startGameBtn.addEventListener('click', () => {
     startGameBtn.style.visibility = "hidden"
     getWords()
     startCountdown()
+    wordInputEl.autofocus = true;
 })
 
 wordInputEl.addEventListener('input', (e) => {
@@ -50,6 +63,10 @@ wordInputEl.addEventListener('input', (e) => {
         getWords()
         wordInputEl.value = ""
         score++
+        scoreEl.textContent = score
+        timeLeft += 4
+        timeLeftEl.innerText = `${timeLeft}s`
     }
 })
+
 
